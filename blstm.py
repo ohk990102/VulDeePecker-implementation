165b1b51d5008a3792d3af90aa0255808d2e476c
ohk990102 <ohk990102@naver.com>
2020-11-25 11:16:59 +0900

    [Fix] some mistakes on blstm, add training code

--- blstm.py
+++ blstm.py
@@ -2,6 +2,8 @@
 https://github.com/yunjey/pytorch-tutorial/blob/master/tutorials/02-intermediate/bidirectional_recurrent_neural_network/main.py
 '''
 
+from cgd import CGDDataset
+import numpy
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -11,13 +13,13 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Hyper-parameters
 sequence_length = 50    # Sequence length -> Paper sets it to 
-input_size = 50         # ?
-hidden_size = 20
+input_size = 100        # ?
+hidden_size = 300
 num_layers = 2
-num_classes = 2
+num_classes = 1
 drouput = 0.5
 batch_size = 100
-num_epochs = 2
+num_epochs = 4
 learning_rate = 1.0
 
 class BLSTM(nn.Module):
@@ -26,7 +28,7 @@ class BLSTM(nn.Module):
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, bidirectional=True)
-        self.fc = nn.Linear(hidden_size*2, num_classes)
+        self.fc = nn.Linear(hidden_size*2, hidden_size*2)
         self.dropout = nn.Dropout(drouput)
     
     def forward(self, x):
@@ -41,5 +43,29 @@ class BLSTM(nn.Module):
 
 model = BLSTM(input_size, hidden_size, num_layers, num_classes).to(device)
 
-loss = nn.CrossEntropyLoss()
+criterion  = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adamax(model.parameters(), lr=learning_rate)
+
+print('[*] Loading dataset...')
+cgd_dataset = CGDDataset('./VulDeePecker/CWE-119/CGD/cwe119_cgd.txt', 100)
+print('[+] Loading dataset complete')
+data_loader = torch.utils.data.DataLoader(cgd_dataset, batch_size=1, shuffle=True)
+
+total_step = len(data_loader)
+
+print('[*] Training model...')
+for epoch in range(num_epochs):
+    for i, (data, label) in enumerate(data_loader):
+        label = torch.from_numpy(numpy.array(label)).cuda(device)
+        output = model(data.cuda(device))
+        loss = criterion(output, label)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i+1) % 100 == 0:
+            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
+                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+
+print('[+] Training model complete')
