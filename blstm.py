cb7dd4502a56476206d31db74028983194ec54d6
ohk990102 <ohk990102@naver.com>
2020-11-29 15:01:05 +0900

    [Fix] cache module deps

--- blstm.py
+++ blstm.py
@@ -8,6 +8,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from cache import load_cache, store_cache
+
 # Check if cuda is available
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
