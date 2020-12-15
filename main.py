77127f4d1034b4223b2e8bcacb6f38d7ccbea6b7
ohk990102 <ohk990102@naver.com>
2020-12-16 04:35:18 +0900

    [Add] more command line features

--- main.py
+++ main.py
@@ -41,19 +41,46 @@ def calc_weight(dataset):
 if __name__ == "__main__":
     import argparse
 
+    config = DefaultTrainConfig()
+
     parser = argparse.ArgumentParser(
         description="VulDeePecker implemented in pytorch. "
     )
-    parser.add_argument("cmd", choices=["train"])
-    parser.add_argument("category", choices=list(setting["dataset"].keys()))
-    result = parser.parse_args()
+    subparser = parser.add_subparsers(
+        help="cmd", dest="cmd", description="Command to run. "
+    )
+    subparser.required = True
 
+    train_group = subparser.add_parser("cv", help="Run cross validation. ")
+    train_group.add_argument("category", choices=list(setting["dataset"].keys()))
+    train_group.add_argument("-l", "--num-layers", nargs="?", type=int)
+    train_group.add_argument("-d", "--dropout", nargs="?", type=float)
+    train_group.add_argument("-b", "--batch-size", nargs="?", type=int)
+    train_group.add_argument("-e", "--num-epochs", nargs="?", type=int)
+    train_group.add_argument("-lr", "--learning-rate", nargs="?", type=float)
+    train_group.add_argument("-k", "--k-fold", nargs="?", type=int)
+    train_group.add_argument("-t", "--test-size", nargs="?", type=float)
+    train_group.add_argument("-o", "--output", nargs="?", type=argparse.FileType("w"))
+
+    result = parser.parse_args()
     cmd = result.cmd
     category = result.category
 
-    config = DefaultTrainConfig()
+    if cmd == "cv":
+        parameters = [
+            "num_layers",
+            "dropout",
+            "batch_size",
+            "num_epochs",
+            "learning_rate",
+            "k_fold",
+            "test_size",
+        ]
+        for parameter in parameters:
+            val = getattr(result, parameter)
+            if val is not None:
+                setattr(config, parameter, val)
 
-    if cmd == "train":
         print("[*] loading dataset...")
         dataset = load_dataset(category)
         print("[+] loading dataset complete")
@@ -70,31 +97,34 @@ if __name__ == "__main__":
         fitter = Fitter(model, device, config)
         total_result = fitter.cross_validation(dataset)
 
-        with open("./cross_val.csv", "w") as f:
-            f.write(
-                "fold,epoch,train_f1,val_f1,train_acc,val_acc,train_recall,val_recall,train_loss,val_loss\n"
-            )
-            for fold, fold_result in enumerate(total_result):
-                for epoch, (
-                    train_summary_loss,
-                    train_total_score,
-                    val_summary_loss,
-                    val_total_score,
-                ) in enumerate(fold_result):
-                    print(
-                        fold + 1,
-                        epoch + 1,
-                        train_total_score.f1,
-                        val_total_score.f1,
-                        train_total_score.precision,
-                        val_total_score.precision,
-                        train_total_score.recall,
-                        val_total_score.recall,
-                        train_summary_loss.avg,
-                        val_summary_loss.avg,
-                        sep=",",
-                        file=f,
-                    )
+        if result.output is None:
+            f = open("./cross_val.csv", "w")
+        else:
+            f = result.output
+        f.write(
+            "fold,epoch,train_f1,val_f1,train_acc,val_acc,train_recall,val_recall,train_loss,val_loss\n"
+        )
+        for fold, fold_result in enumerate(total_result):
+            for epoch, (
+                train_summary_loss,
+                train_total_score,
+                val_summary_loss,
+                val_total_score,
+            ) in enumerate(fold_result):
+                print(
+                    fold + 1,
+                    epoch + 1,
+                    train_total_score.f1,
+                    val_total_score.f1,
+                    train_total_score.precision,
+                    val_total_score.precision,
+                    train_total_score.recall,
+                    val_total_score.recall,
+                    train_summary_loss.avg,
+                    val_summary_loss.avg,
+                    sep=",",
+                    file=f,
+                )
+        f.close()
 
         print("[*] training model complete...")
-        print(result)
